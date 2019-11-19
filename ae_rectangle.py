d74971fb5ce17a1d96f27de77174e5005007275c
Hong, Hyosung <hhs8314@gmail.com>
2019-11-19 17:33:40 +0900

    changed image size

--- ae_rectangle.py
+++ ae_rectangle.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 import cv2
-HEIGHT, WIDTH = 512, 512
+HEIGHT, WIDTH = 128, 128
 img = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
 # points = np.array([[100, 100],[100,200],[200,200],[200,100]])
 center = np.array([[300],[300]])
-rec_size = (100,100)
+rec_size = (30,30)
 
 def get_points(rec_size, center, theta):
     points = np.matrix([
@@ -22,7 +22,7 @@ def get_points(rec_size, center, theta):
     T = np.vstack((T, np.array([0, 0, 1])))
     points = T*points
     return points[:2].transpose().astype(np.int32)
-for x, y, theta in zip(np.random.rand(10)*512, np.random.rand(10)*512, np.random.rand(10)*90):
+for x, y, theta in zip(np.random.rand(10)*128, np.random.rand(10)*128, np.random.rand(10)*90):
     center = np.array([[x], [y]])
     img = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
     cv2.fillPoly(img, [get_points(rec_size, center, theta)], color=(255,255,255))
@@ -30,4 +30,4 @@ for x, y, theta in zip(np.random.rand(10)*512, np.random.rand(10)*512, np.random
     # cv2.waitKey(0)
     if cv2.waitKey(0) & 0xFF == ord('q'):
         break
-
+cv2.destroyAllWindows()
