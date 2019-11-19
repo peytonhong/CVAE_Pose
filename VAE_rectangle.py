9cd0baa4f5a11f609a6cc7b3b8d25bd83da79654
Hong, Hyosung <hhs8314@gmail.com>
2019-11-19 17:32:08 +0900

    Variational AE for rectangle images (toy example)

---
+++ VAE_rectangle.py
@@ -0,0 +1,252 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 10:03:16 2019
+
+@author: hhs83
+"""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+
+import os
+import time
+import numpy as np
+import glob
+import matplotlib.pyplot as plt
+import PIL
+import imageio
+import cv2
+from IPython import display
+
+
+#from tensorflow.compat.v1 import ConfigProto
+#from tensorflow.compat.v1 import InteractiveSession
+#
+#config = ConfigProto()
+#config.gpu_options.allow_growth = True
+#session = InteractiveSession(config=config)
+
+
+#(train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
+#
+#train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
+#test_images = test_images.reshape(test_images.shape[0], 28, 28, 1).astype('float32')
+def get_points(rec_size, center, theta):
+        points = np.matrix([
+                            [-rec_size[0]/2, -rec_size[1]/2],
+                            [-rec_size[0]/2, +rec_size[1]/2],
+                            [+rec_size[0]/2, +rec_size[1]/2],
+                            [+rec_size[0]/2, -rec_size[1]/2]])
+        points = np.vstack((points.transpose(), np.ones((1,4))))
+        theta = theta*np.pi/180
+        R = np.matrix([[np.cos(theta), -np.sin(theta)], 
+                      [np.sin(theta),  np.cos(theta)]])
+        
+        T = np.hstack((R, center))
+        T = np.vstack((T, np.array([0, 0, 1])))
+        points = T*points
+        return points[:2].transpose().astype(np.int32)
+
+def get_rectangles(NUM_IMAGES, WIDTH, HEIGHT):
+    imgs = []
+    rec_size = (30,30)        
+    for x, y, theta in zip(np.random.rand(NUM_IMAGES)*WIDTH, np.random.rand(NUM_IMAGES)*HEIGHT, np.random.rand(NUM_IMAGES)*90):
+        center = np.array([[x], [y]])
+        img = np.zeros((HEIGHT, WIDTH, 3), dtype=np.uint8)
+        cv2.fillPoly(img, [get_points(rec_size, center, theta)], color=(255,255,255))
+        imgs.append(img)
+    return np.array(imgs)
+
+def generate_dataset(BATCH_SIZE):
+    images = get_rectangles(BATCH_SIZE, 128, 128)
+    images = images.astype(np.float32) / 255.
+    images[images >= .5] = 1.
+    images[images < .5] = 0.
+    dataset = tf.data.Dataset.from_tensor_slices(images).batch(BATCH_SIZE)
+    
+    return dataset
+
+test_dataset = generate_dataset(100)
+#train_images = get_rectangles(10000, 512, 512)
+#test_images = get_rectangles(10000, 512, 512)
+## Normalizing the images to the range of [0., 1.]
+#train_images /= 255.
+#test_images /= 255.
+#
+## Binarization
+#train_images[train_images >= .5] = 1.
+#train_images[train_images < .5] = 0.
+#test_images[test_images >= .5] = 1.
+#test_images[test_images < .5] = 0.
+#
+#TRAIN_BUF = 60000
+#BATCH_SIZE = 100
+#
+#TEST_BUF = 10000
+#
+#train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(TRAIN_BUF).batch(BATCH_SIZE)
+#test_dataset = tf.data.Dataset.from_tensor_slices(test_images).shuffle(TEST_BUF).batch(BATCH_SIZE)
+
+
+class CVAE(tf.keras.Model):
+  def __init__(self, latent_dim):
+    super(CVAE, self).__init__()
+    self.latent_dim = latent_dim
+    self.inference_net = tf.keras.Sequential(
+      [
+          tf.keras.layers.InputLayer(input_shape=(128, 128, 3)),
+          tf.keras.layers.Conv2D(
+              filters=128, kernel_size=5, strides=(2, 2), padding="SAME", activation='relu'), # 64x64
+          tf.keras.layers.Conv2D(
+              filters=256, kernel_size=5, strides=(2, 2), padding="SAME", activation='relu'), # 32x32
+          tf.keras.layers.Conv2D(
+              filters=256, kernel_size=5, strides=(2, 2), padding="SAME", activation='relu'), # 16x16
+          tf.keras.layers.Conv2D(
+              filters=512, kernel_size=5, strides=(2, 2), padding="SAME", activation='relu'), # 8x8
+          tf.keras.layers.Flatten(),
+          # No activation
+          tf.keras.layers.Dense(latent_dim + latent_dim),
+      ]
+    )
+
+    self.generative_net = tf.keras.Sequential(
+        [
+          tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+          tf.keras.layers.Dense(units=8*8*512, activation=tf.nn.relu),
+          tf.keras.layers.Reshape(target_shape=(8, 8, 512)),
+          tf.keras.layers.Conv2DTranspose(
+              filters=512, kernel_size=5, strides=(2, 2), padding="SAME", activation='relu'),
+          tf.keras.layers.Conv2DTranspose(
+              filters=256, kernel_size=5, strides=(2, 2), padding="SAME", activation='relu'),
+          tf.keras.layers.Conv2DTranspose(
+              filters=256, kernel_size=5, strides=(2, 2), padding="SAME", activation='relu'),
+          tf.keras.layers.Conv2DTranspose(
+              filters=128, kernel_size=5, strides=(2, 2), padding="SAME", activation='relu'),
+          # No activation
+          tf.keras.layers.Conv2DTranspose(
+              filters=3, kernel_size=3, strides=(1, 1), padding="SAME"),
+        ]
+    )
+
+  @tf.function
+  def sample(self, eps=None):
+    if eps is None:
+      eps = tf.random.normal(shape=(100, self.latent_dim))
+    return self.decode(eps, apply_sigmoid=True)
+
+  def encode(self, x):
+    mean, logvar = tf.split(self.inference_net(x), num_or_size_splits=2, axis=1)
+    return mean, logvar
+
+  def reparameterize(self, mean, logvar):
+    eps = tf.random.normal(shape=mean.shape)
+    return eps * tf.exp(logvar * .5) + mean
+
+  def decode(self, z, apply_sigmoid=False):
+    logits = self.generative_net(z)
+    if apply_sigmoid:
+      probs = tf.sigmoid(logits)
+      return probs
+
+    return logits
+
+optimizer = tf.keras.optimizers.Adam(1e-4)
+
+def log_normal_pdf(sample, mean, logvar, raxis=1):
+  log2pi = tf.math.log(2. * np.pi)
+  return tf.reduce_sum(
+      -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi),
+      axis=raxis)
+
+@tf.function
+def compute_loss(model, x):
+  mean, logvar = model.encode(x)
+  z = model.reparameterize(mean, logvar)
+  x_logit = model.decode(z)
+
+  cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+  logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+  logpz = log_normal_pdf(z, 0., 0.)
+  logqz_x = log_normal_pdf(z, mean, logvar)
+  return -tf.reduce_mean(logpx_z + logpz - logqz_x)
+
+@tf.function
+def compute_apply_gradients(model, x, optimizer):
+  with tf.GradientTape() as tape:
+    loss = compute_loss(model, x)
+  gradients = tape.gradient(loss, model.trainable_variables)
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+  
+epochs = 100
+latent_dim = 50
+num_examples_to_generate = 16
+
+# keeping the random vector constant for generation (prediction) so
+# it will be easier to see the improvement.
+random_vector_for_generation = tf.random.normal(
+    shape=[num_examples_to_generate, latent_dim])
+model = CVAE(latent_dim)
+
+def generate_and_save_images(model, epoch, test_input):
+  predictions = model.sample(test_input)
+  fig = plt.figure(figsize=(4,4))
+
+  for i in range(predictions.shape[0]):
+      plt.subplot(4, 4, i+1)
+      plt.imshow(predictions[i, :, :, 0], cmap='gray')
+      plt.axis('off')
+
+  # tight_layout minimizes the overlap between 2 sub-plots
+  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+  plt.show()
+  
+#generate_and_save_images(model, 0, random_vector_for_generation)
+
+for epoch in range(1, epochs + 1):
+  train_dataset = generate_dataset(100)
+  start_time = time.time()
+  for train_x in train_dataset:
+    compute_apply_gradients(model, train_x, optimizer)
+  end_time = time.time()
+
+  if epoch % 1 == 0:
+    loss = tf.keras.metrics.Mean()
+    for test_x in test_dataset:
+      loss(compute_loss(model, test_x))
+    elbo = -loss.result()
+    display.clear_output(wait=False)
+    print('Epoch: {}, Test set ELBO: {}, '
+          'time elapse for current epoch {}'.format(epoch,
+                                                    elbo,
+                                                    end_time - start_time))
+    generate_and_save_images(
+        model, epoch, random_vector_for_generation)
+    
+def display_image(epoch_no):
+  return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+
+plt.imshow(display_image(epochs))
+plt.axis('off')# Display images
+
+anim_file = 'cvae.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
+  filenames = glob.glob('image*.png')
+  filenames = sorted(filenames)
+  last = -1
+  for i,filename in enumerate(filenames):
+    frame = 2*(i**0.5)
+    if round(frame) > round(last):
+      last = frame
+    else:
+      continue
+    image = imageio.imread(filename)
+    writer.append_data(image)
+  image = imageio.imread(filename)
+  writer.append_data(image)
+
+import IPython
+if IPython.version_info >= (6,2,0,''):
+  display.Image(filename=anim_file)
+  
