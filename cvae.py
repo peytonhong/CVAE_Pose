8fa8f9655551ea9d5ef21d6a891772d9c1d64381
Hong, Hyosung <hhs8314@gmail.com>
2019-12-01 20:57:44 +0900

    Add files via upload

---
+++ cvae.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+import numpy as np
+
+class CVAE(tf.keras.Model):
+    def __init__(self, latent_dim):
+        super(CVAE, self).__init__()
+        self.latent_dim = latent_dim
+        self.inference_net = tf.keras.Sequential(
+        [
+            tf.keras.layers.InputLayer(input_shape=(112, 112, 1)),
+            tf.keras.layers.Conv2D(
+                filters=16, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Conv2D(
+                filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Conv2D(
+                filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
+            tf.keras.layers.Flatten(),
+            # No activation
+            tf.keras.layers.Dense(latent_dim + latent_dim),
+        ]
+        )
+
+        self.generative_net = tf.keras.Sequential(
+            [
+            tf.keras.layers.InputLayer(input_shape=(latent_dim,)),
+            tf.keras.layers.Dense(units=7*7*32, activation=tf.nn.relu),
+            tf.keras.layers.Reshape(target_shape=(7, 7, 32)),
+            tf.keras.layers.Conv2DTranspose(
+                filters=64,
+                kernel_size=3,
+                strides=(2, 2),
+                padding="SAME",
+                activation='relu'),
+            tf.keras.layers.Conv2DTranspose(
+                filters=32,
+                kernel_size=3,
+                strides=(2, 2),
+                padding="SAME",
+                activation='relu'),
+            tf.keras.layers.Conv2DTranspose(
+                filters=16,
+                kernel_size=3,
+                strides=(2, 2),
+                padding="SAME",
+                activation='relu'),
+            # No activation
+            tf.keras.layers.Conv2DTranspose(
+                filters=1, kernel_size=3, strides=(2, 2), padding="SAME"),
+            ]
+        )
+
+        self.pose_net = tf.keras.Sequential(
+            [
+            tf.keras.layers.InputLayer(input_shape=(latent_dim*2,)),
+            tf.keras.layers.Dense(units=100),
+            tf.keras.layers.Dense(units=1)
+            ]
+        )
+
+
+    @tf.function
+    def sample(self, eps=None):
+        if eps is None:
+            eps = tf.random.normal(shape=(100, self.latent_dim))
+        return self.decode(eps, apply_sigmoid=True)
+
+    def encode(self, x):
+        mean, logvar = tf.split(self.inference_net(x), num_or_size_splits=2, axis=1)
+        return mean, logvar
+
+    def reparameterize(self, mean, logvar):
+        eps = tf.random.normal(shape=mean.shape)
+        return eps * tf.exp(logvar * .5) + mean
+
+    def decode(self, z, apply_sigmoid=False):
+        logits = self.generative_net(z)
+        if apply_sigmoid:
+            probs = tf.sigmoid(logits)
+            return probs
+
+        return logits
+
+
+
+def log_normal_pdf(sample, mean, logvar, raxis=1):
+    log2pi = tf.math.log(2. * np.pi)
+    return tf.reduce_sum(
+        -.5 * ((sample - mean) ** 2. * tf.exp(-logvar) + logvar + log2pi), # Monte Carlo estimate of single sample
+        axis=raxis)
+
+@tf.function
+def compute_loss(model, x, y, pose_gt):
+    mean, logvar = model.encode(x)
+    z = model.reparameterize(mean, logvar)
+    x_logit = model.decode(z)
+    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=y)
+    
+    pose_est = model.pose_net(tf.reshape(tf.stack([mean, logvar], axis=1), (-1, model.latent_dim*2)))
+    pose_loss = tf.keras.losses.MSE(pose_est, pose_gt)
+    pose_loss = tf.dtypes.cast(pose_loss, dtype=tf.float32)
+    
+    # ELBO를 계산하는 방법 중 Jensen's Inequality 방식을 적용한 수식: Monte Carlo estimator 방식 사용 (for simplicity)
+    logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
+    logpz = log_normal_pdf(z, 0., 0.)                       # 수식이 좀 이상하다. z를 normal distribution과 닮아가도록 제한하는 역할
+    logqz_x = log_normal_pdf(z, mean, logvar)               # 
+    return -tf.reduce_mean(logpx_z + logpz - logqz_x - pose_loss)
+
+@tf.function
+def compute_apply_gradients(model, x, y, pose_gt, optimizer):
+    with tf.GradientTape() as tape:
+        loss = compute_loss(model, x, y, pose_gt)
+    gradients = tape.gradient(loss, model.trainable_variables)
+    optimizer.apply_gradients(zip(gradients, model.trainable_variables))
---
+++ rectangles.py
@@ -0,0 +1,56 @@
+import numpy as np
+import cv2
+
+""" Generate Rectangles """
+def get_points(rec_size, center, theta):
+        points = np.matrix([
+                            [-rec_size[0]/2, -rec_size[1]/2],
+                            [-rec_size[0]/2, +rec_size[1]/2],
+                            [+rec_size[0]/2, +rec_size[1]/2],
+                            [+rec_size[0]/2, -rec_size[1]/2]])
+        points = np.vstack((points.transpose(), np.ones((1,4))))
+        theta = theta*np.pi/180
+        R = np.matrix([[np.cos(theta), -np.sin(theta)], 
+                      [np.sin(theta),  np.cos(theta)]])
+        
+        T = np.hstack((R, center))
+        T = np.vstack((T, np.array([0, 0, 1])))
+        points = T*points
+        return points[:2].transpose().astype(np.int32)
+
+def get_rectangles(NUM_IMAGES, HEIGHT, WIDTH):  
+    imgs_input = []
+    imgs_output = []
+    theta_list = []
+    rec_size_output = (30,30)
+    for x, y, theta in zip(np.random.rand(NUM_IMAGES)*WIDTH, np.random.rand(NUM_IMAGES)*HEIGHT, np.random.rand(NUM_IMAGES)*90):
+        random_size = int(np.random.rand(1)*20 + 20)   # random integer number between 20 and 40 
+        rec_size_input = (random_size, random_size)
+        center_input = np.array([[x], [y]])
+        center_output = np.array([[56], [56]])
+        theta_list.append(theta)
+        img_input = np.zeros((HEIGHT, WIDTH, 1), dtype=np.uint8)
+        img_output = np.zeros((HEIGHT, WIDTH, 1), dtype=np.uint8)
+        cv2.fillPoly(img_input,  [get_points(rec_size_input,  center_input,  theta)], color=(255,255,255))
+        cv2.fillPoly(img_output, [get_points(rec_size_output, center_output, theta)], color=(255,255,255))
+        imgs_input.append(img_input)
+        imgs_output.append(img_output)
+    return np.array(imgs_input), np.array(imgs_output), np.array(theta_list)
+
+def generate_dataset(BATCH_SIZE):
+    '''
+    Input image: rectangle images of various {position, size, rotation}.
+    Output image: rectangle images of one {position, size} but various rotation angles as input image.
+    '''
+    HEIGHT, WIDTH = 112, 112
+    images_input, images_output, theta_list = get_rectangles(BATCH_SIZE, HEIGHT, WIDTH)
+    images_input = images_input.astype(np.float32) / 255.
+    images_output = images_output.astype(np.float32) / 255.
+    images_input[images_input >= .5] = 1.
+    images_input[images_input < .5] = 0.
+    images_output[images_output >= .5] = 1.
+    images_output[images_output < .5] = 0.
+    # dataset = tf.data.Dataset.from_tensor_slices(images).batch(BATCH_SIZE)    
+    
+    # return images.reshape((-1,HEIGHT*WIDTH))
+    return images_input, images_output, theta_list
